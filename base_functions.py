f1edd92023ceb153ddfdec47d256279d889fae4e
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-03-18 16:32:24 +0530

    added grey level function and texture analysis

--- base_functions.py
+++ base_functions.py
@@ -10,13 +10,24 @@ if __name__ == "__main__":
     plt.imshow(data, cmap='gray')
     plt.show()
 
-def load_image(filename):
+def load_image(filename): # give filename inside Dataset folder, returns numpy array
     f = h5py.File('Dataset/' + filename, 'r')
     dset = f['VHRR']['Image Data']['VHRR_TIR']
     print(dset)
     data = np.array(dset[:,:])
     return data
 
-def show_image(data):
+def show_image(data): # plot image using matplotlib, returns plot
     plt.imshow(data, cmap='gray')
     plt.show()
+
+def plot_hist(data): # plot histogram
+    plt.hist(data.ravel())
+    plt.show()
+
+def plot_hist_bin(data, bins): # plot histogram with bins
+    plt.hist(data.ravel(), bins=bins, histtype="bar")
+    plt.show()
+
+def get_grey_level_array(data): # returns 16-bin distribution
+    return np.histogram(data.ravel(), bins=16)[0]
--- process.py
+++ process.py
@@ -1,4 +1,18 @@
 import base_functions as bf
+import numpy as np
+import skimage.feature as sk
 
-data = bf.load_image("test1.h5")
-bf.show_image(data)
+# load image
+data = bf.load_image("test2.h5")
+bf.plot_hist_bin(data, 16)
+
+# grey-level array
+gla = bf.get_grey_level_array(data)
+
+# texture analysis
+glcm = sk.greycomatrix(data, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], 1024)
+con = sk.greycoprops(glcm, 'contrast')
+enr = sk.greycoprops(glcm, 'energy')
+cor = sk.greycoprops(glcm, 'correlation')
+dis = sk.greycoprops(glcm, 'dissimilarity')
+print(con, enr, cor, dis)
